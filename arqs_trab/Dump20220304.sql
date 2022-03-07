9456901328c383d8c49f5ffb7135fddb64d7ff29
FJAConceicao <lipeconceicao13@gmail.com>
2022-03-06 22:49:21 -0300

    Importaçao do bd completo do trabalho para o site
    Adiçao dos arquivos do trabalho na pasta raiz

---
+++ arqs_trab/Dump20220304.sql
@@ -0,0 +1,225 @@
+-- MySQL dump 10.13  Distrib 5.7.37, for Linux (x86_64)
+--
+-- Host: localhost    Database: cake_cms
+-- ------------------------------------------------------
+-- Server version	5.7.37-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP DATABASE IF EXISTS cake_cms;
+
+CREATE DATABASE IF NOT EXISTS cake_cms;
+
+USE cake_cms;
+
+--
+-- Table structure for table `info`
+--
+
+DROP TABLE IF EXISTS `info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dimension` varchar(256) DEFAULT NULL,
+  `weight` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info`
+--
+
+LOCK TABLES `info` WRITE;
+/*!40000 ALTER TABLE `info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manufacturer`
+--
+
+DROP TABLE IF EXISTS `manufacturer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manufacturer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `url` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manufacturer`
+--
+
+LOCK TABLES `manufacturer` WRITE;
+/*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `asin` varchar(256) DEFAULT NULL,
+  `brand` varchar(256) DEFAULT NULL,
+  `source_url` varchar(2048) DEFAULT NULL,
+  `description` varchar(2048) DEFAULT NULL,
+  `fk_info_info_PK` int(11) DEFAULT NULL,
+  `fk_Manufacturer_Id` int(11) DEFAULT NULL,
+  `categories` varchar(2048) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Product_3` (`fk_info_info_PK`),
+  KEY `FK_Product_4` (`fk_Manufacturer_Id`),
+  CONSTRAINT `FK_Product_3` FOREIGN KEY (`fk_info_info_PK`) REFERENCES `info` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `FK_Product_4` FOREIGN KEY (`fk_Manufacturer_Id`) REFERENCES `manufacturer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_User_Id` int(11) DEFAULT NULL,
+  `fk_Product_Id` int(11) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `did_purchase` tinyint(1) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `text` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_Review_2` (`fk_User_Id`),
+  KEY `FK_Review_3` (`fk_Product_Id`),
+  CONSTRAINT `FK_Review_2` FOREIGN KEY (`fk_User_Id`) REFERENCES `user` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `FK_Review_3` FOREIGN KEY (`fk_Product_Id`) REFERENCES `product` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review`
+--
+
+LOCK TABLES `review` WRITE;
+/*!40000 ALTER TABLE `review` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sells`
+--
+
+DROP TABLE IF EXISTS `sells`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sells` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_Product_Id` int(11) DEFAULT NULL,
+  `fk_Store_Id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_sells_1` (`fk_Product_Id`),
+  KEY `FK_sells_2` (`fk_Store_Id`),
+  CONSTRAINT `FK_sells_1` FOREIGN KEY (`fk_Product_Id`) REFERENCES `product` (`id`),
+  CONSTRAINT `FK_sells_2` FOREIGN KEY (`fk_Store_Id`) REFERENCES `store` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sells`
+--
+
+LOCK TABLES `sells` WRITE;
+/*!40000 ALTER TABLE `sells` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sells` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) DEFAULT NULL,
+  `url` varchar(2048) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store`
+--
+
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(256) DEFAULT NULL,
+  `city` varchar(256) DEFAULT NULL,
+  `province` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-04 13:11:31
---
+++ arqs_trab/bd_copy.sql
@@ -0,0 +1,99 @@
+DROP DATABASE IF EXISTS cake_cms;
+
+CREATE DATABASE IF NOT EXISTS cake_cms;
+
+USE cake_cms;
+
+CREATE TABLE product ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256), 
+    asin VARCHAR(256), 
+    brand VARCHAR(256), 
+    source_url VARCHAR(2048), 
+    description VARCHAR(2048), 
+    fk_info_info_PK INT, 
+    fk_Manufacturer_Id INT 
+) AUTO_INCREMENT = 1; 
+ 
+CREATE TABLE user ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    username varchar(256), 
+    city varchar(256), 
+    province varchar(256) 
+) AUTO_INCREMENT = 1; 
+ 
+CREATE TABLE manufacturer ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256), 
+    url VARCHAR(2048) 
+) AUTO_INCREMENT = 1; 
+ 
+CREATE TABLE store ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256), 
+    url VARCHAR(2048) 
+) AUTO_INCREMENT = 1; 
+ 
+CREATE TABLE categorie (
+    id INT PRIMARY KEY NOT NULL,
+    categorie VARCHAR(256),
+    product_fk INT
+); 
+ 
+CREATE TABLE info ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    dimension VARCHAR(256),
+    weight VARCHAR(256)
+) AUTO_INCREMENT = 1; 
+ 
+CREATE TABLE review ( 
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fk_User_Id INT, 
+    fk_Product_Id INT,  
+    date DATETIME, 
+    did_purchase BOOLEAN, 
+    rating INT, 
+    title VARCHAR(256), 
+    text VARCHAR(2048) 
+) AUTO_INCREMENT = 1;
+ 
+CREATE TABLE sells (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fk_Product_Id INT, 
+    fk_Store_Id INT 
+) AUTO_INCREMENT = 1;
+   
+ALTER TABLE product ADD CONSTRAINT FK_Product_3 
+    FOREIGN KEY (fk_info_info_PK) 
+    REFERENCES info (id) 
+    ON DELETE SET NULL; 
+  
+ALTER TABLE product ADD CONSTRAINT FK_Product_4 
+    FOREIGN KEY (fk_Manufacturer_Id) 
+    REFERENCES manufacturer (id) 
+    ON DELETE RESTRICT; 
+  
+ALTER TABLE review ADD CONSTRAINT FK_Review_2 
+    FOREIGN KEY (fk_User_Id) 
+    REFERENCES user (id) 
+    ON DELETE SET NULL; 
+  
+ALTER TABLE review ADD CONSTRAINT FK_Review_3 
+    FOREIGN KEY (fk_Product_Id) 
+    REFERENCES product (id) 
+    ON DELETE SET NULL; 
+  
+ALTER TABLE sells ADD CONSTRAINT FK_sells_1 
+    FOREIGN KEY (fk_Product_Id) 
+    REFERENCES product (id) 
+    ON DELETE RESTRICT; 
+  
+ALTER TABLE sells ADD CONSTRAINT FK_sells_2 
+    FOREIGN KEY (fk_Store_Id) 
+    REFERENCES store (id) 
+    ON DELETE SET NULL; 
+    
+ALTER TABLE categorie ADD CONSTRAINT FK_categorie_1 
+    FOREIGN KEY (product_fk) 
+    REFERENCES product (id) 
+    ON DELETE SET NULL; 
